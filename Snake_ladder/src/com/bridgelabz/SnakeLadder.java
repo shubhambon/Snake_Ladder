1a5cf40339674fa2d8010ab2ea35c2ea189ae8ad
shubham <shubham.bondre.1995@gmail.com>
2022-10-08 08:29:45 +0530

    Check for a option

--- Snake_ladder/src/com/bridgelabz/SnakeLadder.java
+++ Snake_ladder/src/com/bridgelabz/SnakeLadder.java
@@ -1,18 +1,26 @@
 package com.bridgelabz;
 
-import java.util.Random;
-
 public class SnakeLadder {
     public static void main(String[] args) {
-        Random r = new Random();
-        System.out.println("Welcome to snake ladder program.");
-        int userPosition = 0;
-        System.out.println("----Game Starts----");
-        System.out.println("Initial position of user: "+userPosition);
 
-        int dice = r.nextInt(6) + 1 ;
-        System.out.println("Dice Rolled: "+dice);
+        int position = 0;
+        System.out.println(" Player Checks for a option");
+
+        int randomv = (int) Math.floor(Math.random() * 10) % 6 + 1;
+        System.out.println(" Number Got: " + randomv);
+
+        int option = (int) Math.floor(Math.random() * 10) % 3;
+
+        if (option == 0) {
+            System.out.println(" Player Action : No Play");
+        } else if (option == 1) {
+            System.out.println(" Player Action : Ladder");
+            position += randomv;
+        } else if (option == 2) {
+            System.out.println(" Player Action : Snake");
+            position -= randomv;
+        }
+
+        System.out.println("Current Position" + position);
     }
-
 }
-
