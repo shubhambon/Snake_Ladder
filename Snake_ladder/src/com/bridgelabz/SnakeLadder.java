b4a272daee33dc0094bcb7a27c40eb82ea01275f
shubham <shubham.bondre.1995@gmail.com>
2022-10-08 08:47:49 +0530

    Game With 2 Player

--- Snake_ladder/src/com/bridgelabz/SnakeLadder.java
+++ Snake_ladder/src/com/bridgelabz/SnakeLadder.java
@@ -5,17 +5,21 @@ import java.util.Scanner;
 
 public class SnakeLadder {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-
+        System.out.println("Welcome Player");
         int position = 0;
+        int position2 = 0;
         int dice;
         int option;
         int roll = 0;
-        System.out.println("Welcome Player");
+        int turn = 0;
+        int oldTurn = 0;
 
-        while (position != 100) {
-            //System.out.println("Enter 1 to roll a die");
-            // int die = in.nextInt();
+        while (position < 100 && position2 < 100) {
+
+            if (turn == 0)
+                System.out.println("Player 1 is Playing");
+            else
+                System.out.println("Player 2 is Playing ");
             dice = (int) Math.floor((Math.random() * 10) % 6 + 1);
             System.out.println("\n The dice rolled to: " + dice);
             option = (int) Math.floor(Math.random() * 3);
@@ -26,25 +30,54 @@ public class SnakeLadder {
                     break;
                 case 1:
                     System.out.println("Player action: Ladder");
-                    position += dice;
+                    if (turn == 0)
+                        position += dice;
+                    else
+                        position2 += dice;
+
                     if (position > 100) {
                         position -= dice;
                         System.out.println("Player exceeds 100");
                     }
+
+                    if (position2 > 100) {
+                        position2 -= dice;
+                        System.out.println("Player2 exceeds 100");
+                    }
                     break;
+
                 case 2:
                     System.out.println("Player Action: Snake");
-                    position -= dice;
-                    position = Math.max(position, 0);
+                    if (turn != 0)
+                        position -= dice;
+                    else
+                        position2 -= dice;
+
+                    if (position < 0) {
+                        position = Math.max(position, 0);
+                        System.out.println("Player1 reset to 0");
+                    }
+
+                    if (position2 < 0) {
+                        position2 = Math.max(position2, 0);
+                        System.out.println("Player2 reset to 0");
+                    }
+
+                    turn = (turn + 1) % 2;
                     break;
             }
-
-
-            System.out.println("Current position:" + position);
+            if (oldTurn == 0)
+                System.out.println("Current position for Player1: " + position + "\n");
+            else
+                System.out.println("Current position for Player2: " + position2 + "\n");
             roll++;
+            oldTurn = turn;
+
         }
-        System.out.println("Congratulations!! You took " + roll + " rolls to win the game!!");
+        System.out.println("Congratulations Player " + (turn + 1) + "!!");
+        System.out.println("We took " + roll + " rolls to win the game!!");
     }
 }
+
 
 
