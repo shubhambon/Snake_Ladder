c21f048fdd5deadcd8ff08d4689436ccd6092681
shubham <shubham.bondre.1995@gmail.com>
2022-10-08 08:18:10 +0530

    Start The Game

---
+++ Snake_ladder/src/com/bridgelabz/SnakeLadder.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class SnakeLadder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to snake ladder program.");
+        int userPosition = 0;
+        System.out.println("----Game Starts----");
+        System.out.println("Initial position of user: "+userPosition);
+
+    }
+
+}
+
